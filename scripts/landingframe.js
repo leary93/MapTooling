66a9e7bf226b55ce44887b729b34da8724871dfc
Leroy <leroyodk@gmail.com>
2020-01-30 12:25:46 +0100

    *Fixed a-href bug
    Signed-off-by: Leroy <leroyodk@gmail.com>

--- scripts/landingframe.js
+++ scripts/landingframe.js
@@ -1,24 +1,27 @@
 Vue.component('landing-frame',{
   template:`
     <div class="column is-one-quarter is-centered">
-      <a v-bind:href="tool.link" target="toolframe" onclick="LandingToggle(true)">
         <div class="add-border box is-relative" >
-          <figure class="image is-16by9">
-            <img v-bind:src="tool.image" v-bind:alt="tool.name"></img>
-          </figure>
+          <a v-bind:href="tool.link" target="toolframe" onclick="LandingToggle(true)">
+            <figure class="image is-16by9">
+              <img v-bind:src="tool.image" v-bind:alt="tool.name"></img>
+            </figure>
+          </a>
           <div class="field has-addons">
             <p class="control" v-if="tool.video">
               <button class="videobutton button is-transparent" @click.stop="openVideo"><i class="fab fa-youtube"></i></button>
             </p>
             <p class="control is-expanded">
-              <button class="is-fullwidth button has-text-centered has-text-weight-semibold is-radiusless is-paddingless"> {{ tool.text }} </button>
+              <a v-bind:href="tool.link" target="toolframe" onclick="LandingToggle(true)">
+                <button class="is-fullwidth button has-text-centered has-text-weight-semibold is-radiusless is-paddingless"> {{ tool.text }} </button>
+              </a>
             </p>
             <p class="control" v-if="tool.info">
               <button class="infobutton button is-transparent" @click.stop="openInfo"><i class="fas fa-info"></i></button>
             </p>
           </div>
         </div>
-      </a>
+
     </div>
     `,
     props: ['tool'],
